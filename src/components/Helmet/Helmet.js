62166ed2a35188bf5f8c506d99fee804b83df105
Apurv Patil <patilapurv18@gmail.com>
2023-06-06 22:39:03 +0530

    Update the  name to Green Foenix

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Helmet = (props) => {
-  document.title = "G-Mart -" + props.title;
+  document.title = "Green Foenix -" + props.title;
   return <div className="w-100">{props.children}</div>;
 };
 
